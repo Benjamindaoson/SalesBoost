56e9efde48684893d34035ede48eaeb1747d9eaa
Benjamin Daoson <jianming001@e.ntu.edu.sg>
2026-01-21 19:41:08 +0800

    Add offline shims and testing fallbacks

---
+++ langchain_core/__init__.py
@@ -0,0 +1,4 @@
+from .output_parsers import PydanticOutputParser
+from .prompts import ChatPromptTemplate, PromptTemplate
+
+__all__ = ["ChatPromptTemplate", "PromptTemplate", "PydanticOutputParser"]
